4c9e6ba38507291e87d5f978fa4c610c422cee04
윤영광 <66102223+glory-yun@users.noreply.github.com>
2026-04-21 12:23:58 +0900

    Title: 문자열 정렬, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260421/문자열 정렬/string-sort.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        char[] arr = str.toCharArray();
+
+        Arrays.sort(arr);
+
+        str = new String(arr);
+
+
+        System.out.println(str);
+
+    }
+}
