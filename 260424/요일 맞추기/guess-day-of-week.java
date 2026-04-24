f05dca6869c077d496a92ef55d28986df75e3ad4
윤영광 <66102223+glory-yun@users.noreply.github.com>
2026-04-24 15:35:15 +0900

    Title: 요일 맞추기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/요일 맞추기/guess-day-of-week.java
+++ 260424/요일 맞추기/guess-day-of-week.java
@@ -1,56 +1,36 @@
 import java.util.*;
 
 public class Main {
-
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
+
         int[] month = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
         String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        String[] days2 = {"Mon", "Sun", "Sat", "Fri", "Thu", "Wed", "Tue"};
 
-        int cnt = 0;
-
-
-        if (m1 <= m2 && d1 <= d2) {
-            while (true) {
-                cnt++;
-                d1++;
-
-                if (m1 == m2 && d1 == d2) break;
-
-                if (d1 == month[m1]) {
-                    m1++;
-                    d1 = 1;
-                }
-            }
-
-            cnt %= 7;
-
-            System.out.println(days[cnt]);
-
-
-        } else if (m1 >= m2 && d1 >= d2) {
-            while (true) {
-                cnt++;
-                d2++;
-
-                if (m1 == m2 && d1 == d2) break;
-
-                if (d2 == month[m2]) {
-                    m2++;
-                    d2 = 1;
-                }
-            }
-
-            cnt %= 7;
-            System.out.println(days2[cnt]);
-
-
+        int cnt1 = d1;
+        int cnt2 = d2;
+        int month1 = 1, month2 = 1;
+        while (month1 < m1) {
+            cnt1 += month[month1++];
         }
+
+        while (month2 < m2) {
+            cnt2 += month[month2++];
+        }
+
+        int diff =cnt2-cnt1;
+
+        diff %= 7;
+        diff = diff < 0 ? diff +=7 : diff;
+
+        System.out.println(days[diff]);
+
+
     }
 }
