a0a678bc54f000dee8b341a4151a3c394ed114ee
윤영광 <66102223+glory-yun@users.noreply.github.com>
2026-04-24 14:30:50 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/DateTime to DateTime/datetime-to-datetime.java
+++ 260424/DateTime to DateTime/datetime-to-datetime.java
@@ -12,7 +12,7 @@ public class Main {
 		int h = 11;
 		int m = 11;
 
-		if (!(d <= day && h <= hour && m <= minute)) {
+		if (day < d || (day==d && hour < h) || (day==d && hour == h && minute < m)) {
 			System.out.println(-1);
 			return;
 		}
